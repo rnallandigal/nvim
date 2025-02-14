b6183590469e1eb93e24460e1309046f246159eb
Rohith Nallandigal <rohith.nallandigal@altruist.com>
2025-02-13 16:47:08 -0800

    Use telescope for lsp introspections
    Signed-off-by: Rohith Nallandigal <rohith.nallandigal@altruist.com>

--- lua/community.lua
+++ lua/community.lua
@@ -1,9 +1,4 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
 }
